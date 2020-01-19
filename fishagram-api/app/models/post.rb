636304bbf47dc969d38282e9f0893e760c6957e2
Kenneth Spatta <kenyounot@gmail.com>
2020-01-18 20:00:22 -0500

    added validations for the Post model

--- fishagram-api/app/models/post.rb
+++ fishagram-api/app/models/post.rb
@@ -2,9 +2,15 @@ include Rails.application.routes.url_helpers
 
 class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
-
   has_one_attached :image, dependent: :destroy
 
+  validates :caption, :length, :weight, :lure_used, presence: true
+  validates :caption, length: { maximum: 140 }
+  validates :length, :weight, numericality: { only_integer: true }
+  validates :lure_used, length: { maximum: 20 }
+
+
+  
   def image_url
     url_for(self.image)
   end
