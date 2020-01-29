1ec0d2303e73140ffe4af22426960fadcbb4e2b4
Kenneth Spatta <kenyounot@gmail.com>
2020-01-29 11:49:27 -0500

    removed comment update controller action as it was not needed

--- fishagram-api/app/controllers/api/v1/comments_controller.rb
+++ fishagram-api/app/controllers/api/v1/comments_controller.rb
@@ -18,30 +18,7 @@ class Api::V1::CommentsController < ApplicationController
         end
     end
 
-    def update
-        if @comment = Comment.find_by(id: params[:id])
-            if @comment.update(comment_params)
-                render json: {
-                    comment: @comment,
-                    updated: true
-                }
-
-            else
-                render json: {
-                    comment: @comment,
-                    errors: @comment.errors,
-                    updated: false
-                }
-            end
-
-        else
-            render json: {
-                updated: false,
-                errors: 'Comment not found'
-            }
-        end
-    end
-
+ 
     def destroy
         if @comment = Comment.find_by(id: params[:id])
             if @comment.delete
